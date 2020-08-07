a514aea3cbec902a858ff8f656dba0be9cd1b8aa
dev-juri <ojurifemi132@gmail.com>
2020-08-07 20:43:51 +0100

    created details screen

---
+++ app/src/main/java/com/oluwafemi/demonewsapp/details/DetailFragment.kt
@@ -0,0 +1,27 @@
+package com.oluwafemi.demonewsapp.details
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.oluwafemi.demonewsapp.databinding.DetailFragmentBinding
+
+class DetailFragment : Fragment() {
+
+    val viewModel : DetailViewModel by lazy {
+        ViewModelProvider(this).get(DetailViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DetailFragmentBinding.inflate(inflater)
+        binding.viewModel = viewModel
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/com/oluwafemi/demonewsapp/details/DetailViewModel.kt
@@ -0,0 +1,18 @@
+package com.oluwafemi.demonewsapp.details
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+
+class DetailViewModel : ViewModel() {
+
+    private val viewModelJob = Job()
+
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+}
